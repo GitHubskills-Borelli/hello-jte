28ec2cfa4550e3de9c53a38cc2b92a22e3ab52a1
Borelli-7 <kaly.salas.7@gmail.com>
2024-10-05 16:59:59 +0100

    init commit

--- src/main/java/dev/danvega/jte/HelloController.java
+++ src/main/java/dev/danvega/jte/HelloController.java
@@ -12,9 +12,12 @@ public class HelloController {
     @GetMapping("/")
     public String home(Model model) {
         var page = new Page("Hello, World", "This is my first home page!");
-        var items = List.of("Item 1","Item 2","Item 3");
+        var items = new java.util.ArrayList<>(List.of("Item 1", "Item 2", "Item 3"));
+        var cni = 1234567890;
+        items.clear();
 
         model.addAttribute("name","Dan");
+        model.addAttribute("cni", cni);
         model.addAttribute("items",items);
         model.addAttribute("page",page);
 
